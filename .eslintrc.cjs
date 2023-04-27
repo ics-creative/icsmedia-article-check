18bd62396d1f55c3d26f6f90cb18000089a98abe
ics-kitagawa <kitagawa@ics-web.jp>
2023-04-27 13:10:44 +0900

    依存関係の追加、tsconfigのmoduleをCommonJSにする

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ["dist", ".eslintrc.*"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking"
+  ],
+  rules: {
+    "import/prefer-default-export": "off",
+    "@typescript-eslint/quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "semi-spacing": ["error", {"after": true, "before": false}],
+    "semi-style": ["error", "last"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error"
+  },
+};
