1ba3bba5f6b95a58a3a6f0a3c268596a7200ab9b
ics-kitagawa <kitagawa@ics-web.jp>
2023-04-27 18:42:06 +0900

    ファイルが存在しなかった場合の考慮

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,9 +1,16 @@
 import fs from "node:fs";
+import process from "process";
+import {LOG_ERROR} from "../consts/consts";
 
 /**
  * 引数で受け取ったパスのファイルを読み込みます
  * @param filePath ファイルパス
  */
 export const readFile = (filePath: string) => {
-  return fs.readFileSync(filePath, "utf-8");
+  try {
+    return fs.readFileSync(filePath, "utf-8");
+  } catch (e) {
+    console.warn(`${LOG_ERROR}ファイルが存在しません。${filePath}`);
+    process.exit(1);
+  }
 };
