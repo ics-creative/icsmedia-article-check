93d06c34cf2e13fb696eb4c94ab0a9760425052d
ics-sawada-h <haruka.sawada@ics-web.jp>
2023-04-28 11:00:00 +0900

    PR修正：見出しレベル5はエラーにする

--- src/logics/articleCheck/articleHeadingLevelCheck.ts
+++ src/logics/articleCheck/articleHeadingLevelCheck.ts
@@ -3,17 +3,17 @@ import {printErrorLog} from "../../utils/printErrorLog";
 /** 見出しの正規表現 */
 const PATTERN_BASE = /\n#+.+/g;
 
-/** 正常に表示されない見出しの正規表現（見出しレベルに1,2,6~を設定しているパターン） */
-const PATTERN_ERROR_LEVEL = /\n(#{1,2}|#{6,})(\s|\u3000|[^#]).+/g;
+/** 正常に表示されない見出しの正規表現（見出しレベルに1,2,5~を設定しているパターン） */
+const PATTERN_ERROR_LEVEL = /\n(#{1,2}|#{5,})(\s|\u3000|[^#]).+/g;
 
 /** 正常に表示されない見出しの正規表現（#の後に全角スペースが続くパターン） */
-const PATTERN_ERROR_SPACE = /\n#{3,5}\u3000.+/g;
+const PATTERN_ERROR_SPACE = /\n#{3,4}\u3000.+/g;
 
 /** 正常に表示されない見出しの正規表現（#の後にスペースがないパターン） */
-const PATTERN_ERROR_ZERO_SPACE = /\n#{3,5}[^#|\s]+/g;
+const PATTERN_ERROR_ZERO_SPACE = /\n#{3,4}[^#|\s]+/g;
 
 /** 正常に表示されない見出しの正規表現（タグが入るパターン） */
-const PATTERN_ERROR_TAG = /\n#{3,5}[^`]*<.+>[^`]*/g;
+const PATTERN_ERROR_TAG = /\n#{3,4}[^`]*<.+>[^`]*/g;
 
 /**
  * 使用できない見出しのパターンを使用していないかチェックを行います。
@@ -30,19 +30,19 @@ const checkErrorHeading = (text: string): string[] => {
       case PATTERN_ERROR_LEVEL.test(heading):
         // 連続でエラーが続くと一つ飛ばしにしか表示されないのでmatchさせて回避しています
         heading.match(PATTERN_ERROR_LEVEL);
-        return "使用できない見出しレベルです\n" + removeNewline(heading);
+        return "使用できない見出しレベルです。\n" + removeNewline(heading);
 
       case PATTERN_ERROR_SPACE.test(heading):
         heading.match(PATTERN_ERROR_SPACE);
-        return "全角スペースが含まれています\n" + removeNewline(heading);
+        return "全角スペースが含まれています。\n" + removeNewline(heading);
 
       case PATTERN_ERROR_ZERO_SPACE.test(heading):
         heading.match(PATTERN_ERROR_ZERO_SPACE);
-        return "#の直後に半角スペースがありません\n" + removeNewline(heading);
+        return "#の直後に半角スペースがありません。\n" + removeNewline(heading);
 
       case PATTERN_ERROR_TAG.test(heading):
         heading.match(PATTERN_ERROR_TAG);
-        return "見出しに素の<>タグは使えません\n" + removeNewline(heading);
+        return "見出しに素の<>タグは使えません。\n" + removeNewline(heading);
 
       default:
         return "";
