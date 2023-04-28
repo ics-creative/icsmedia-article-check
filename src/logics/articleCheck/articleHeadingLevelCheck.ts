c5b5e55652a32e52b3315f7224d50e4ecbca5146
ics-sawada-h <haruka.sawada@ics-web.jp>
2023-04-28 17:35:42 +0900

    PR修正：見当てはまるエラーは全て表示するよう修正

--- src/logics/articleCheck/articleHeadingLevelCheck.ts
+++ src/logics/articleCheck/articleHeadingLevelCheck.ts
@@ -3,17 +3,17 @@ import {printErrorLog} from "../../utils/printErrorLog";
 /** 見出しの正規表現 */
 const PATTERN_BASE = /\n#+.+/g;
 
-/** 正常に表示されない見出しの正規表現（見出しレベルに1,2,5~を設定しているパターン） */
+/** 見出しレベルにh3とh4以外を設定しているパターンの正規表現 */
 const PATTERN_ERROR_LEVEL = /\n(#{1,2}|#{5,})(\s|\u3000|[^#]).+/g;
 
-/** 正常に表示されない見出しの正規表現（#の後に全角スペースが続くパターン） */
-const PATTERN_ERROR_SPACE = /\n#{3,4}\u3000.+/g;
+/** #の直後に全角スペースが続くパターンの正規表現 */
+const PATTERN_ERROR_SPACE = /\n#+\u3000.+/g;
 
-/** 正常に表示されない見出しの正規表現（#の後にスペースがないパターン） */
-const PATTERN_ERROR_ZERO_SPACE = /\n#{3,4}[^#|\s]+/g;
+/** #の直後に半角スペースがないパターンの正規表現 */
+const PATTERN_ERROR_ZERO_SPACE = /\n#+[^#|\s]+/g;
 
-/** 正常に表示されない見出しの正規表現（タグが入るパターン） */
-const PATTERN_ERROR_TAG = /\n#{3,4}[^`]*<.+>[^`]*/g;
+/** 素の<>タグが入るパターンの正規表現（`<input>`のようにバッククォートで囲んでいない）*/
+const PATTERN_ERROR_TAG = /\n#+[^`]*<.+>[^`]*/g;
 
 /**
  * 使用できない見出しのパターンを使用していないかチェックを行います。
@@ -26,35 +26,28 @@ const checkErrorHeading = (text: string): string[] => {
   }
 
   return headings.map((heading) => {
-    switch (true) {
-      case PATTERN_ERROR_LEVEL.test(heading):
-        // 連続でエラーが続くと一つ飛ばしにしか表示されないのでmatchさせて回避しています
-        heading.match(PATTERN_ERROR_LEVEL);
-        return "使用できない見出しレベルです。\n" + removeNewline(heading);
-
-      case PATTERN_ERROR_SPACE.test(heading):
-        heading.match(PATTERN_ERROR_SPACE);
-        return "全角スペースが含まれています。\n" + removeNewline(heading);
-
-      case PATTERN_ERROR_ZERO_SPACE.test(heading):
-        heading.match(PATTERN_ERROR_ZERO_SPACE);
-        return "#の直後に半角スペースがありません。\n" + removeNewline(heading);
-
-      case PATTERN_ERROR_TAG.test(heading):
-        heading.match(PATTERN_ERROR_TAG);
-        return "見出しに素の<>タグは使えません。\n" + removeNewline(heading);
-
-      default:
-        return "";
+    const errorMessages = [];
+    if (PATTERN_ERROR_LEVEL.test(heading)) {
+      // 連続でエラーが続くと一つ飛ばしにしか表示されないのでmatchさせて回避しています
+      heading.match(PATTERN_ERROR_LEVEL);
+      errorMessages.push("\n\x1b[31m使用できない見出しレベルです。\x1b[39m");
     }
-  }).filter(heading => heading);
-};
-
-/**
- * 改行コードを取り除きます。
- * */
-const removeNewline = (text: string): string => {
-  return text.replace(/\n/g, "");
+    if (PATTERN_ERROR_SPACE.test(heading)) {
+      heading.match(PATTERN_ERROR_SPACE);
+      errorMessages.push("\n\x1b[31m全角スペースが含まれています。\x1b[39m");
+    }
+    if (PATTERN_ERROR_ZERO_SPACE.test(heading)) {
+      heading.match(PATTERN_ERROR_ZERO_SPACE);
+      errorMessages.push("\n\x1b[31m#の直後に半角スペースがありません。\x1b[39m");
+    }
+    if (PATTERN_ERROR_TAG.test(heading)) {
+      heading.match(PATTERN_ERROR_TAG);
+      errorMessages.push("\n\x1b[31m見出しに素の<>タグは使えません。\x1b[39m");
+    }
+    // エラー判定の場合のみ該当の見出しを表示する
+    const errorHeading = errorMessages.length > 0 ? heading : "";
+    return errorMessages.join(",").replace(/,/g, "") + errorHeading;
+  }).filter(heading => heading); // 空の文字列を除外する
 };
 
 /**
