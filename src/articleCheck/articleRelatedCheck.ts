da181a223d89004973efb8ae815242d7f14c2989
ics-sawada-h <haruka.sawada@ics-web.jp>
2023-05-01 12:00:16 +0900

    PR修正：リファクタリング

--- src/articleCheck/articleRelatedCheck.ts
+++ src/articleCheck/articleRelatedCheck.ts
@@ -1,42 +1,54 @@
 import {printErrorLog} from "../utils/printErrorLog";
 
-/** 関連記事の行の正規表現（文字列内で一番最初のrelated:のみ） */
-const PATTERN_RELATED = /related:.+\n/;
+/** 関連記事の正規表現（マークダウンファイル冒頭のrelated:） */
+const PATTERN_RELATED = /---(.|\n)+related:.+\n(.|\n)+---/;
 
-/** カンマ区切りの数字と英語の単語の正規表現（数字|「tutorial-」から始まる文字以外はエラー） */
-const PATTERN_COMMAS = /(\d+|tutorial-\w+)(?:\s*,\s*(\d+|tutorial-\w+))*/g;
+/** 関連記事の行の正規表現 */
+const PATTERN_RELATED_LINE = /related:.+\n/;
+
+/** カンマ区切りの「英数字と-_」の単語の正規表現 */
+const PATTERN_COMMAS = /[\w-]+(?:\s*,\s*[\w-]+)*/g;
 
 /**
  * 関連記事の個数を検証します。
  * */
-const countRelated = (text: string): string => {
+const countRelated = (text: string): string[] => {
+  // 記事冒頭のrelated:を抽出
+  const related = text.match(PATTERN_RELATED);
+  if (!related) {
+    return ["関連記事が存在しません。"];
+  }
   // 関連記事の行を抽出
-  const relatedLine = text.match(PATTERN_RELATED);
+  const relatedLine = related[0].match(PATTERN_RELATED_LINE);
   if (!relatedLine) {
-    return "";
+    return [];
   }
-
-  // 関連記事が正しく表記されているか検証（数字|「tutorial-」から始まる文字以外はエラー）
-  const relatedArticles = relatedLine[0].match(PATTERN_COMMAS);
+  // 「related:〜」以降の関連記事を抽出
+  const relatedArticles = relatedLine[0].replace(/related:/, "").match(PATTERN_COMMAS);
   if (!relatedArticles) {
-    return "";
+    return [];
   }
-  // エラーパターンを除いてまとめる
-  const extractedRelatedArticles = relatedArticles.join(",");
+  // 文字列にまとめる
+  const relatedArticlesString = arrayToString(relatedArticles);
   // カンマ区切りで分割し、余分なスペースを取り除いて返す
-  const relatedArticlesArray = extractedRelatedArticles.split(",").map(value => value.trim());
+  const extractedRelatedArticles = relatedArticlesString.split(",").map(value => value.trim());
 
-  return generateMessage(relatedArticlesArray);
+  return generateMessage(extractedRelatedArticles);
 };
 
 /**
- * 関連記事の個数をチェックします。
+ * 関連記事の個数は所定の個数か？
  * */
-const checkRelatedLength = (relatedArray: string[]): string => {
-  // 関連記事の所定の本数
-  const PREDETERMINED_RELATED_NUMBER = 8;
+const isValidLength = (relatedArray: string[]): boolean => {
+  return relatedArray.length === 8;
+};
+
+/**
+ * 関連記事と所定の個数の差分
+ * */
+const checkDifference = (relatedArray: string[]): string => {
   // 所定の関連記事の本数との差分
-  const difference = Math.abs(PREDETERMINED_RELATED_NUMBER - relatedArray.length);
+  const difference = Math.abs(8 - relatedArray.length);
   if (relatedArray.length > 8) {
     return `関連記事が${difference}点多いです。\n`;
   } else if (relatedArray.length < 8) {
@@ -47,11 +59,11 @@ const checkRelatedLength = (relatedArray: string[]): string => {
 };
 
 /**
- * 関連記事の重複をチェックします。
+ * 関連記事の重複
  */
 const checkDuplication = (relatedArray: string[]): string => {
-  const result = [...new Set(relatedArray)];
-  if (relatedArray.length !== result.length) {
+  const noDuplicationArray = [...new Set(relatedArray)];
+  if (relatedArray.length !== noDuplicationArray.length) {
     return "関連記事が重複しています。\n";
   } else {
     return "";
@@ -59,21 +71,20 @@ const checkDuplication = (relatedArray: string[]): string => {
 };
 
 /**
- * エラーメッセージ用に配列を文字列に結合します。
+ * エラーメッセージの表示用に配列を文字列に結合します。
  * */
-const toStringForErrorMessage = (relatedArray: string[]) => {
-  // 配列を文字列に連結
-  return relatedArray.join(",");
+const arrayToString = (array: string[]): string => {
+  return array.join(",");
 };
 
 /**
  * エラーメッセージを生成します。
  * */
-const generateMessage = (relatedArray: string[]) => {
-  if (checkRelatedLength(relatedArray) || checkDuplication(relatedArray)) {
-    return checkRelatedLength(relatedArray) + checkDuplication(relatedArray) + toStringForErrorMessage(relatedArray);
+const generateMessage = (relatedArray: string[]): string[] => {
+  if (isValidLength(relatedArray)) {
+    return [];
   } else {
-    return "";
+    return [checkDifference(relatedArray) + checkDuplication(relatedArray) + arrayToString(relatedArray)];
   }
 };
 
@@ -82,7 +93,6 @@ const generateMessage = (relatedArray: string[]) => {
  * @param mdFile マークダウン形式の記事
  * */
 export const articleRelatedCheck = (mdFile: string) => {
-  const errorMessages = countRelated(mdFile).length > 0 ? [countRelated(mdFile)] : [];
-
+  const errorMessages = countRelated(mdFile);
   printErrorLog(["関連記事の個数チェックを行います。"], errorMessages);
 };
