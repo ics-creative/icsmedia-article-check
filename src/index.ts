da117dcf095c3f3773600b10bf5b9f3f6c0deae6
ics-kitagawa <kitagawa@ics-web.jp>
2023-05-02 14:45:37 +0900

    対話型のプロンプトを受け取る

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,35 @@
 #!/usr/bin/env node
 
-import {getPath} from "./utils/getPath";
-import {readFile} from "./utils/readFile";
-import {MD_NAME} from "./consts/consts";
-import {toHtml} from "./utils/toHtml";
-import {articleHeadingLevelCheck} from "./logics/articleCheck/articleHeadingLevelCheck";
-import {expiredLinkCheck} from "./logics/expiredLinkCheck/expiredLinkCheck";
-import {fileCapacityCheck} from "./logics/fileCapacityCheck/fileCapacityCheck";
-import {eyecatchCheck} from "./logics/eyecatchCheck/eyecatchCheck";
-import {articleDateCheck} from "./logics/articleCheck/articleDateCheck";
-import {articleRelatedCheck} from "./logics/articleCheck/articleRelatedCheck";
+import { getPath } from "./utils/getPath";
+import { readFile } from "./utils/readFile";
+import { MD_NAME } from "./consts/consts";
+import { toHtml } from "./utils/toHtml";
+import { articleHeadingLevelCheck } from "./logics/articleCheck/articleHeadingLevelCheck";
+import { expiredLinkCheck } from "./logics/expiredLinkCheck/expiredLinkCheck";
+import { fileCapacityCheck } from "./logics/fileCapacityCheck/fileCapacityCheck";
+import { eyecatchCheck } from "./logics/eyecatchCheck/eyecatchCheck";
+import { articleDateCheck } from "./logics/articleCheck/articleDateCheck";
+import { articleRelatedCheck } from "./logics/articleCheck/articleRelatedCheck";
 
-// チェックするファイルがあるディレクトリのパスを取得
-const basePath = getPath();
-// マークダウンファイルを読み込み
-const mdFile = readFile(`${basePath}/${MD_NAME}`);
-// HTML形式に変換
-const html = toHtml(mdFile);
+const validate = async () => {
+  // チェックするファイルがあるディレクトリのパスを取得
+  const basePath = await getPath();
+  // マークダウンファイルを読み込み
+  const mdFile = readFile(`${basePath}/${MD_NAME}`);
+  // HTML形式に変換
+  const html = toHtml(mdFile);
+  // リンク切れチェック
+  void expiredLinkCheck(html);
+  // 容量チェック
+  fileCapacityCheck(basePath);
+  // アイキャッチの検証
+  void eyecatchCheck(basePath);
+  // htmlに変換される前の見出しレベルのチェック
+  articleHeadingLevelCheck(mdFile);
+  // 関連記事の個数チェック
+  articleRelatedCheck(mdFile);
+  // 公開日と更新日の整合性チェック
+  articleDateCheck(html);
+};
 
-// リンク切れチェック
-void expiredLinkCheck(html);
-
-// 容量チェック
-fileCapacityCheck(basePath);
-
-// アイキャッチの検証
-void eyecatchCheck(basePath);
-
-// htmlに変換される前の見出しレベルのチェック
-articleHeadingLevelCheck(mdFile);
-
-// 関連記事の個数チェック
-articleRelatedCheck(mdFile);
-
-// 公開日と更新日の整合性チェック
-articleDateCheck(html);
+void validate();
