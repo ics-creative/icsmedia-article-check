2274dcae57945809dda33ee20c4a4beea149edf1
ics-sawada-h <haruka.sawada@ics-web.jp>
2023-05-09 11:49:15 +0900

    PR修正：#１個から始まる行はエラーとして検出しない

--- src/logics/articleCheck/articleHeadingLevelCheck.ts
+++ src/logics/articleCheck/articleHeadingLevelCheck.ts
@@ -1,36 +1,36 @@
 import { printErrorLog } from "../../utils/printErrorLog";
 
 /** 使用できない見出しを抽出するための正規表現 */
-const PATTERN_ERROR = /(<h[1256]>(?<!title:).+<\/h[1256]>)|(\n<p>#+.+)/g;
+const PATTERN_ERROR = /(<h[1256]>(?<!title:).+<\/h[1256]>)|(\n<p>#{2,}.+)/g;
 
 /** 使用できないレベルのhタグの正規表現 */
 const PATTERN_H_ERROR_LEVEL = /<h[1256]>(?<!title:).+<\/h[1256]>/g;
 
 /** #の直後に全角スペースが続いてpタグに変換されたパターン */
-const PATTERN_P_ERROR_SPACE = /<p>#+\u3000.+/g;
+const PATTERN_P_ERROR_SPACE = /<p>#{2,}\u3000.+/g;
 
 /** #の直後に半角スペースがなかったためpタグに変換されたパターン */
-const PATTERN_P_ERROR_ZERO_SPACE = /<p>#+[^#|\s]+/g;
+const PATTERN_P_ERROR_ZERO_SPACE = /<p>#{2,}[^#|\s]+/g;
 
 /** pタグに変換された後、h3とh4以外を設定しようとしていたパターン */
-const PATTERN_P_ERROR_LEVEL = /<p>(#{1,2}|#{5,})(\s|\u3000|[^#]).+/g;
+const PATTERN_P_ERROR_LEVEL = /<p>(#{2}|#{5,})(\s|\u3000|[^#]).+/g;
 
 /**
  * 使用できない見出しのパターンを使用していないかチェックを行います。
  * */
 const checkErrorHeadings = (text: string): string[] => {
   // 使用できない見出しを抽出
-  const errors =text.match(PATTERN_ERROR);
+  const errors = text.match(PATTERN_ERROR);
 
   const errorHeadings: string[] = [];
   errors?.forEach((heading) => {
     const errorMessages = [];
-    // 使用できない見出しレベルの場合を抽出
+    // 使用できない見出しレベルを抽出
     if(PATTERN_H_ERROR_LEVEL.test(heading)){
       errorMessages.push("使用できない見出しレベルです。h3とh4が使用できます。\n");
     }
 
-    // 見出しタグに変換できてない場合を抽出（pタグで#が使用されている場合）
+    // 見出しタグに変換できてないが文頭に#が連続で使われていて、見出しにする予定だったと思われる場合を抽出
     if (PATTERN_P_ERROR_SPACE.test(heading)) {
       // 連続でエラーが続くと一つ飛ばしにしか表示されないのでmatchさせて回避
       heading.match(PATTERN_P_ERROR_SPACE);
