6bdae9cfe7969e8d18a5d8ee309d7a732070adeb
ics-kitagawa <kitagawa@ics-web.jp>
2023-05-10 11:58:09 +0900

    リンクチェックで、リクエストとレスポンスのurlが違っていた場合をエラーにする

--- src/logics/expiredLinkCheck/expiredLinkCheck.ts
+++ src/logics/expiredLinkCheck/expiredLinkCheck.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { printErrorLog } from "../../utils/printErrorLog";
 import { parse } from "node-html-parser";
 import { notNull } from "../../utils/notNull";
@@ -18,36 +18,31 @@ export const expiredLinkCheck = async (html: string) => {
   const requests = links.map(link => {
     return new Promise<Response>((resolve, reject) => {
       axios.get(link)
-        .then((res: AxiosResult) => resolve({ link: link, statusCode: res.status, statusText: res.statusText }))
+        .then((res: AxiosResponse) => {
+          const request = res.request as {res: {responseUrl: string}};
+          resolve({ link, resUrl: request.res.responseUrl });
+        })
         .catch((err: Error) => reject({ link: link, message: err.message }));
     });
   });
 
   // 並列で処理を行う
   const results = await Promise.allSettled(requests);
-  // status=200以外のものを抽出
-  const expired = results.filter(res => res.status === "rejected" || res.value.statusCode !== 200);
-
+  // リクエストが失敗したもの or リクエストのurlとレスポンスのurlが違うものを抽出
+  const expired = results.filter(res => res.status === "rejected" || res.value.resUrl !== res.value.link);
   // エラーメッセージを構築
   const messages = expired.map((ex) => {
     return ex.status === "rejected" ?
     // Promiseのrejected.reasonのany型を解決できなかったのでeslintを一時的にdisable
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       `${ex.reason.message as string} link:${ex.reason.link as string}` :
-      `${ex.value.statusCode.toString()} ${ex.value.statusText} link:${ex.value.link}`;
+      `リクエストとレスポンスのurlが異なっています。\nrequest:\n${ex.value.link}\nresponse:\n${ex.value.resUrl}`;
   });
-
   // ログ出力
   printErrorLog(["リンク切れのチェックを行います。"], messages);
 };
 
-type AxiosResult = {
-  status: number,
-  statusText: string,
-}
-
 type Response = {
   link: string,
-  statusCode: number,
-  statusText: string
+  resUrl: string
 }
